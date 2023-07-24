abba2bad85be8cc2548d9068ae3383b46584f599
Jeremy <cayabyabjeremy25@gmail.com>
2023-07-25 00:19:48 +0800

    added course subject & code

--- gui/app.py
+++ gui/app.py
@@ -28,8 +28,14 @@ class App(ct.CTk):
             "BSCS" : {  "1st Year": [
 
                         ], "2nd Year": [
-                            "COMP20113: Technical Documentation and Presentation Skills", "COMP20113", "COMP20113", "COMP20113",
-                            "COMP20113", "COMP20113", "COMP20113", "COMP20113"
+                            "COMP20113: Technical Documentation and Presentation Skills", 
+                            "PHED10042: Team Sports", 
+                            "GEED20113: People and Earth's Ecosystem", 
+                            "COMP20103: Operating Systems",
+                            "COMP20093: Information Management", 
+                            "COSC3033: Design and Analysis of Algorithms", 
+                            "COSC-FE2: CS Free Elective", 
+                            "GEED10073: Art Appreciation"
                         ], "3rd Year": [
 
                         ], "4th Year": [
